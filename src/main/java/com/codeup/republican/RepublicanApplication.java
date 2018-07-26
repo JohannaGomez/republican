090d356422ff02f566ebec554ee95cdb7fda8f89
Johanna Gomez <jgomez.satx@gmail.com>
2018-07-26 17:20:52 -0500

    initial commit

---
+++ src/main/java/com/codeup/republican/RepublicanApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.republican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepublicanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepublicanApplication.class, args);
+	}
+}
